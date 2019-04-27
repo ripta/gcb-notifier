1f2c05051868ff93aa9fd9ea9c5b18cbcf2789a2
Ripta Pasay <ripta@users.noreply.github.com>
2019-04-27 15:43:44 -0700

    index.js: copy from cloud build docs

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const IncomingWebhook = require('@slack/client').IncomingWebhook;
+const SlackWebhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+
+exports.gcbNotifier = (event, cb) => {
+  const build = eventToBuildAdapter(event.data.data);
+  const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
+  if (status.indexOf(build.status) === -1) {
+    return cb();
+  }
+
+  const message = buildSlackMessage(build);
+  SlacWebhook.send(message, cb);
+};
+
+const eventToBuildAdapter = (data) => {
+  return JSON.parse(new Buffer(data, 'base64').toString());
+};
+
+const buildSlackMessage = (build) => {
+  return {
+    text: `Build ${build.id}`,
+    mrkdwn: true,
+    attachments: [
+      {
+        title: 'Build logs',
+        title_link: build.logUrl,
+        fields: [{
+          title: 'Status',
+          value: build.status,
+        }],
+      },
+    ],
+  };
+};
